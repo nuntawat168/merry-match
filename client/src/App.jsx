c6e62aff9d1fc0ad9cd14b9d302ba8bee5b0e060
bambam <chickpiyabam@gmail.com>
2023-09-04 10:38:32 +0700

    feat:ui-package-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
 import "./App.css";
-import LoginPage from "./pages/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
 import { Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import MatchPages from "./pages/MatchPages";
 import MerryListPage from "./pages/MerryListPage";
 import AdminAddPackage from "./pages/AdminAddPackage";
+import Content from "./components/Content";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/match" element={<MatchPages />} />
-        <Route path="/merry-list" element={<MerryListPage />} />
-        <Route path="/addPackage" element={<AdminAddPackage />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/match" element={<MatchPages />} />
+          <Route path="/merry-list" element={<MerryListPage />} />
+          <Route path="/addPackage" element={<AdminAddPackage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
