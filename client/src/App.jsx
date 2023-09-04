116c6b788cdfb29e7182f40d7752685480357875
Panpaphop <panpaphop9@gmail.com>
2023-09-05 00:54:04 +0700

    feat:add login page(only ui)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,19 @@
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
 import RegisterPage from "./pages/RegisterPage";
 import MatchPages from "./pages/MatchPages";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/match" element={<MatchPages />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/match" element={<MatchPages />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
