586870a15285732bb33e3f3fa595f4be160d5c08
Ice Nantawat <nuntawat_168@outlook.com>
2023-09-07 18:14:41 +0700

    feat:add route to register page and match page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import RegisterPage from "./pages/RegisterPage";
+import MatchPages from "./pages/MatchPages";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </BrowserRouter>
-  </>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/match" element={<MatchPages />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
