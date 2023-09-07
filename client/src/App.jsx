8accd9beb7cdde49d88b34508fb931c11671c59e
Panpaphop <panpaphop9@gmail.com>
2023-09-07 18:26:36 +0700

    feat:connect ui login with server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,18 @@
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
+import LandingPage from "./pages/LandingPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import MatchPages from "./pages/MatchPages";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/match" element={<MatchPages />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/match" element={<MatchPages />} />
+    </Routes>
   );
 }
 
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,85 +1,103 @@
-// LoginPage.js
-
-import {React, useState, useEffect} from "react";
+import React, { useState } from "react";
 import manImage from "../../src/assets/image/manOnLoginPage.svg";
 import dotImage from "../../src/assets/image/dotOnLoginPage.svg";
+import Navbar from "../components/Navbar.jsx";
+import { useAuth } from "../contexts/authentication.jsx";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
-  // Define state variables for username and password
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  // Handle changes in the input fields
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    login({
+      email,
+      password,
+    });
   };
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+
   return (
-    <div className="mt-10 flex w-full">
-      <div className="w-1/2 flex justify-center relative">
-        <img src={manImage} className="" alt="manImage" />
-        <img src={dotImage} className="absolute top-0 left-0 " alt="dotImage" />
-      </div>
-      <div className="w-1/2 flex flex-col justify-around items-start pl-10 mt-10 mb-10">
-        <p className="text-purple-500 font-nunito text-[14px] font-semibold uppercase">
-          LOGIN
-        </p>
-        <p className="font-nunito text-[46px] font-extrabold  text-purple-500">
-          Welcome back to
-        </p>
-        <p className="font-nunito text-[46px] font-extrabold  text-purple-500">
-          Merry Match
-        </p>
-        <div className="mt-10 mb-4 flex flex-col items-start">
-          <label
-            htmlFor="usernameOrEmail"
-            className="font-nunito text-[16px] font-normal text-black inline-block"
-          >
-            Username or Email
-          </label>
-          <input
-            type="text"
-            id="usernameOrEmail"
-            placeholder="Enter Username or Email"
-            value={username} // Bind the input value to the username state
-            onChange={handleUsernameChange} // Handle changes in the input field
-            className="w-[453px] h-[48px] border border-[#D6D9E4] rounded-lg mt-2 p-4"
+    <>
+      <Navbar />
+      <div className="mt-20 flex w-full">
+        <div className="w-1/2 flex justify-center relative">
+          <img src={manImage} className="" alt="manImage" />
+          <img
+            src={dotImage}
+            className="absolute top-0 left-0 "
+            alt="dotImage"
           />
         </div>
-        <div className="mb-4 flex flex-col items-start">
-          <label
-            htmlFor="password"
-            className="font-nunito text-[16px] font-normal text-black inline-block"
-          >
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Enter password"
-            value={password} // Bind the input value to the password state
-            onChange={handlePasswordChange} // Handle changes in the input field
-            className="w-[453px] h-[48px] border border-[#D6D9E4] rounded-lg mt-2 p-4"
-          />
-        </div>
-        <button class="w-[453px] h-[48px] rounded-full bg-red-500 hover:bg-red-600 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 shadow-md text-white">
-          Login
-        </button>
-        <div className="flex mt-10">
-          <p class="font-nunito text-base font-normal text-black">
-            Don't have an account?
+        <div className="w-1/2 flex flex-col justify-around items-start pl-10 mt-10 mb-10">
+          <p className="text-purple-500 font-nunito text-[14px] font-semibold uppercase">
+            LOGIN
           </p>
-          <a
-            href=""
-            className="text-red-500 font-nunito text-base font-bold leading-[24px] ml-2"
-            onClick={() => navigate("")}
+          <p className="font-nunito text-[46px] font-extrabold  text-purple-500">
+            Welcome back to
+          </p>
+          <p className="font-nunito text-[46px] font-extrabold  text-purple-500">
+            Merry Match
+          </p>
+          <div className="mt-10 mb-4 flex flex-col items-start">
+            <label
+              htmlFor="email"
+              className="font-nunito text-[16px] font-normal text-black inline-block"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+              className="w-[453px] h-[48px] border border-[#D6D9E4] rounded-lg mt-2 p-4"
+            />
+          </div>
+          <div className="mb-4 flex flex-col items-start">
+            <label
+              htmlFor="password"
+              className="font-nunito text-[16px] font-normal text-black inline-block"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              className="w-[453px] h-[48px] border border-[#D6D9E4] rounded-lg mt-2 p-4"
+            />
+          </div>
+          <button
+            onClick={handleLogin}
+            className="w-[453px] h-[48px] rounded-full bg-red-500 hover:bg-red-600 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 shadow-md text-white"
           >
-            Register
-          </a>
+            Login
+          </button>
+          <div className="flex mt-10">
+            <p className="font-nunito text-base font-normal text-black">
+              Don't have an account?
+            </p>
+            <a
+              href=""
+              className="text-red-500 font-nunito text-base font-bold leading-[24px] ml-2"
+              onClick={() => navigate("")}
+            >
+              Register
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
