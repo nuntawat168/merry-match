9e4a6e1fc3052703bd80b3227964e163105e65be
Apiwat Lee <apiwatlee@Apiwats-MacBook-Air.local>
2023-09-08 17:23:53 +0700

    feat:add get api for packages

--- server/apps/package.js
+++ server/apps/package.js
@@ -60,4 +60,18 @@ packageRouter.post("/", async (req, res) => {
   return res.status(200).json({ message: "Package has been created" });
 });
 
+packageRouter.get("/", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM packages JOIN package_details ON packages.package_id = package_details.package_id;"
+    );
+    return res.json({
+      data: result.rows,
+    });
+  } catch (error) {
+    console.error("เกิดข้อผิดพลาดในการร้องขอข้อมูล:", error);
+    res.status(500).json({ error: "เกิดข้อผิดพลาดในการร้องขอข้อมูล" });
+  }
+});
+
 export default packageRouter;
