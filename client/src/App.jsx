02a14a8b552956b533bcd85d6992ddea3073125f
Panpaphop <panpaphop9@gmail.com>
2023-09-11 15:12:22 +0700

    feat:fix comment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,28 +12,28 @@ import Adminpackage from "../src/pages/Adminpackage";
 import AuthenticatedApp from "./pages/AuthenticatedApp";
 import UnauthenticatedApp from "./pages/UnauthenticatedApp";
 
-// function App() {
-//   return (
-//     <>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/register" element={<RegisterPage />} />
-//         <Route path="/match" element={<MatchPages />} />
-//         <Route path="/merry-list" element={<MerryListPage />} />
-//         <Route path="/addPackage" element={<AdminAddPackage />} />
-//         <Route path="/packages" element={<PackagePage />} />
-//         <Route path="/admin" element={<Adminpackage />} />
-//         {/* <Route path="/payment" element={<PaymentPage1 />} /> */}
-//       </Routes>
-//     </>
-//   );
-// }
-// export default App;
-
 function App() {
-  const auth = useAuth();
-  return auth.isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/match" element={<MatchPages />} />
+        <Route path="/merry-list" element={<MerryListPage />} />
+        <Route path="/addPackage" element={<AdminAddPackage />} />
+        <Route path="/packages" element={<PackagePage />} />
+        <Route path="/admin" element={<Adminpackage />} />
+        {/* <Route path="/payment" element={<PaymentPage1 />} /> */}
+      </Routes>
+    </>
+  );
 }
-
 export default App;
+
+// function App() {
+//   const auth = useAuth();
+//   return auth.isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+// }
+
+// export default App;
