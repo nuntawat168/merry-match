541eb48015dd1fbdef73f083be5d5e379000e567
CodePawsDev <isripen1991@outlook.com>
2023-09-11 19:16:22 +0700

    feat: add noti dropdown

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,17 +4,25 @@ import { Link as RouterLink } from "react-router-dom";
 import { useState } from "react";
 import notiIcon from "../assets/icon/noti.svg";
 import UserDropdown from "./UserDropdown";
+import Notification from "./Notification";
 
 function Navbar() {
     // แก้ เอา hard code ออก ดึงสถานะจริงมาเช็ค
     const [loggedIn, setLoggedIn] = useState(true);
     const [subscribed, setSubscribed] = useState(false);
     const [userDropdown, setUserDropdown] = useState(false);
+    const [notificationDropdown, setNotificationDropdown] = useState(false);
 
     const toggleUserDropdown = () => {
+        setNotificationDropdown(false)
         setUserDropdown(!userDropdown);
     }
     
+    const toggleNotificationDropdown = () => {
+        setUserDropdown(false)
+        setNotificationDropdown(!notificationDropdown);
+    }
+
     // create a rendering condition for each list
     const anchors = [
         { id: 1, destination: loggedIn? "/match" : "whyMerryMatch", text: loggedIn ? "Start Matching!" : "Why Merry Match?" },
@@ -47,8 +55,12 @@ function Navbar() {
                     {/* if logged in show user noti and profile img, else show login btn*/}
                     { loggedIn ? (
                         <div className="flex justify-between ml-[24px]">
-                            <div className="w-[48px] h-[48px] bg-gray-100 rounded-full flex justify-center items-center">
+                            <div 
+                                className="w-[48px] h-[48px] bg-gray-100 rounded-full flex justify-center items-center relative"
+                                onClick={toggleNotificationDropdown}
+                            >
                                 <img src={notiIcon} alt="notification" className="w-[24px] h-[24px]"/>
+                                {notificationDropdown && <Notification />}
                             </div>
                             <div 
                                 className="ml-[12px] w-[48px] h-[48px] rounded-full bg relative"
---
+++ client/src/components/Notification.jsx
@@ -0,0 +1,26 @@
+
+
+function Notification() {
+    const notifications = [
+        {username: "Khal Drogo", content: "Just Merry you! Click here to see profile"},
+        {username: "Daeny", content: "Merry you back! Let's start conversation now"},
+        {username: "Ygritte", content: "Merry you back! Let's start conversation now"},
+    ]
+
+    const renderedNoti = notifications.map((noti, index) => {
+        return (
+            <div key={index} className="flex justify-between py-[12px] px-[14px]">
+                <div className="w-[32px] h-[32px] bg rounded-full"></div>
+                <p className="w-[179px]">'{noti.username}' {noti.content}</p>
+            </div>
+        )
+    })
+
+    return (
+        <div className="z-20 absolute top-[60px] right-[0px] font-nunito text-[14px] text-gray-700 font-normal shadow-userDropdown bg-white rounded-2xl w-[251px] h-[214px] flex flex-col justify-center">
+            {renderedNoti}
+        </div>
+    )
+}
+
+export default Notification;
