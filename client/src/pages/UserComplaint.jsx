46d2e24b42a9f36e51ab1f562fd7118d1c36f34a
Panpaphop <panpaphop9@gmail.com>
2023-09-12 16:21:46 +0700

    feat: add user-complain ui

---
+++ client/src/pages/UserComplaint.jsx
@@ -0,0 +1,125 @@
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup"; // For validation
+import manImage from "../../src/assets/image/manOnLoginPage.svg";
+import dotImage from "../../src/assets/image/dotOnLoginPage.svg";
+import Navbar from "../components/Navbar.jsx";
+import { useAuth } from "../contexts/authentication.jsx";
+import { useNavigate } from "react-router-dom";
+
+function UserComplaintPage() {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  // Define the initial form values
+  const initialValues = {
+    issue: "",
+    description: "",
+    dateSubmitted: "",
+  };
+
+  // Define a validation schema using Yup
+  const validationSchema = Yup.object({
+    issue: Yup.string().required("Issue is required"),
+    description: Yup.string().required("Description is required"),
+    dateSubmitted: Yup.date().required("Date Submitted is required"),
+  });
+
+  // Handle form submission
+  const onSubmit = (values) => {
+    // Perform the form submission logic here
+    console.log(values);
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="mt-20 flex w-full">
+        <div className="w-1/2 flex flex-col justify-start items-center pl-10 mt-10 mb-10 ml-10">
+          <div>
+            <p className="text-purple-500 font-nunito text-[14px] font-semibold uppercase">
+              COMPLAINT
+            </p>
+            <p className="font-nunito text-[46px] font-extrabold text-purple-500">
+              If you have any trouble
+            </p>
+            <p className="font-nunito text-[46px] font-extrabold text-purple-500">
+              Don't be afraid to tell us!
+            </p>
+          </div>
+
+          {/* Use Formik to create the form */}
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            <Form>
+              <div className="mt-10 mb-4 flex flex-col items-start">
+                <label
+                  htmlFor="issue"
+                  className="font-nunito text-[16px] font-normal text-black inline-block"
+                >
+                  Issue
+                </label>
+                <Field
+                  type="text"
+                  id="issue"
+                  name="issue"
+                  placeholder="Enter Issue"
+                  className="w-[548px] h-[48px] border border-[#D6D9E4] rounded-lg mt-2 p-4"
+                />
+                <ErrorMessage name="issue" component="div" className="text-red-500" />
+              </div>
+
+              <div className="mt-10 mb-4 flex flex-col items-start">
+                <label
+                  htmlFor="description"
+                  className="font-nunito text-[16px] font-normal text-black inline-block"
+                >
+                  Description
+                </label>
+                <Field
+                  as="textarea"
+                  id="description"
+                  name="description"
+                  placeholder="Enter Description"
+                  className="w-[548px] h-[196px] border border-[#D6D9E4] rounded-lg mt-2 p-4"
+                />
+                <ErrorMessage name="description" component="div" className="text-red-500" />
+              </div>
+
+              <div className="mt-10 mb-4 flex flex-col items-start">
+                <label
+                  htmlFor="dateSubmitted"
+                  className="font-nunito text-[16px] font-normal text-black inline-block"
+                >
+                  Date Submitted
+                </label>
+                <Field
+                  type="date"
+                  id="dateSubmitted"
+                  name="dateSubmitted"
+                  className="w-[357px] h-[48px] border border-[#D6D9E4] rounded-lg mt-2 p-4"
+                />
+                <ErrorMessage name="dateSubmitted" component="div" className="text-red-500" />
+              </div>
+
+              <button
+                type="submit"
+                className="w-[102px] h-[48px] rounded-full bg-red-500 hover:bg-red-600 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 shadow-md text-white mt-[58px]"
+              >
+                Submit
+              </button>
+            </Form>
+          </Formik>
+        </div>
+        <div className="w-1/2 flex flex justify-center relative">
+          <img src={manImage} className="w-[65%]" alt="manImage" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UserComplaintPage;
