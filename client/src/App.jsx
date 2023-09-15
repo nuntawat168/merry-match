57f0001a068d16eddb1498dd849f7f90eb693a2d
Winteriscoming5976 <thanawat46491@gmail.com>
2023-09-15 12:31:01 +0700

    fix : rebase

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,18 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import MatchPages from "./pages/MatchPages";
-import MerryListPage from "./pages/MerryListPage";
-import AdminAddPackage from "./pages/AdminAddPackage";
-import PackagePage from "./pages/packagepage";
-import Adminpackage from "../src/pages/Adminpackage";
-import PaymentPage1 from "./pages/PaymentPage1";
-import Chat from "./pages/Chat";
-import Test1 from "./pages/Test1";
-import AdminComplaintListPage from "./pages/AdminComplaintListPage";
+// import "./App.css";
+// import { Routes, Route } from "react-router-dom";
+// import LandingPage from "./pages/LandingPage";
+// import LoginPage from "./pages/LoginPage";
+// import RegisterPage from "./pages/RegisterPage";
+// import MatchPages from "./pages/MatchPages";
+// import MerryListPage from "./pages/MerryListPage";
+// import AdminAddPackage from "./pages/AdminAddPackage";
+// import PackagePage from "./pages/packagepage";
+// import Adminpackage from "../src/pages/Adminpackage";
+// import PaymentPage1 from "./pages/PaymentPage1";
+// import Chat from "./pages/Chat";
+// import Test1 from "./pages/Test1";
+// import AdminComplaintListPage from "./pages/AdminComplaintListPage";
 
-function App() {
-
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/match" element={<MatchPages />} />
-        <Route path="/merry-list" element={<MerryListPage />} />
-        <Route path="/addPackage" element={<AdminAddPackage />} />
-        <Route path="/packages" element={<PackagePage />} />
-        <Route path="/admin" element={<Adminpackage />} />
-        <Route path="/payment" element={<PaymentPage1 />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/test" element={<Test1 />} />
-      </Routes>
-    </>
-  );
-}
-export default App;
-
-
-
-// import AuthenticatedApp from "./pages/AuthenticatedApp";
-// import UnauthenticatedApp from "./pages/UnauthenticatedApp";
-// import { useAuth } from "./contexts/authentication";
 
 // function App() {
 
--- server/apps/user.js
+++ server/apps/user.js
@@ -47,34 +47,12 @@ userRouter.get("/matchlist/:user_id", async (req, res) => {
     }
 });
 
-userRouter.get("/unmatchlist/:user_id", async (req, res) => {
-    try {
-        const { user_id } = req.params;
-
-        const result = await pool.query(
-            `SELECT DISTINCT users.user_id, users.sex, users.name, users.age, users.email, users.city, users.location, users.sex_identities, users.sexual_preferences,users.racial_preferences, users.meeting_interests, profile_images.image
-            FROM users
-            INNER JOIN profile_images ON users.user_id = profile_images.user_id
-            INNER JOIN merry_lists ON users.user_id = merry_lists.user_interest OR users.user_id = merry_lists.user_response
-            WHERE (merry_lists.merry_match_status IS NULL OR merry_lists.merry_match_status = FALSE) OR (merry_lists.merry_match_status = TRUE AND users.user_id != $1)
-            `,
-            [user_id]
-
-        );
 
 
 
+        
 
-        );
 
-res.json({
-    data: result.rows,
-});
-    } catch (error) {
-    console.error("เกิดข้อผิดพลาดในการร้องขอข้อมูล:", error);
-    res.status(500).json({ error: "เกิดข้อผิดพลาดในการร้องขอข้อมูล" });
-}
-});
 
 
 
