868f1d020771034bebd981261e36150f0441fbe4
bambam <chickpiyabam@gmail.com>
2023-09-15 14:25:05 +0700

    feat:seach-Bar-Modal-end-for-merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,38 +14,37 @@ import Chat from "./pages/Chat";
 import Test1 from "./pages/Test1";
 import AdminComplaintListPage from "./pages/AdminComplaintListPage";
 
-// function App() {
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/match" element={<MatchPages />} />
+        <Route path="/merry-list" element={<MerryListPage />} />
+        <Route path="/package/add" element={<AdminAddPackage />} />
+        <Route path="/package/edit/:id" element={<AdminEditPackage />} />
+        <Route path="/packages" element={<PackagePage />} />
+        <Route path="/admin" element={<Adminpackage />} />
+        <Route path="/payment" element={<PaymentPage1 />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/test" element={<Test1 />} />
+        <Route path="/Complaint" element={<AdminComplaintListPage />} />
 
-//   return (
-//     <>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/register" element={<RegisterPage />} />
-//         <Route path="/match" element={<MatchPages />} />
-//         <Route path="/merry-list" element={<MerryListPage />} />
-//         <Route path="/package/add" element={<AdminAddPackage />} />
-//         <Route path="/package/edit/:id" element={<AdminEditPackage />} />
-//         <Route path="/packages" element={<PackagePage />} />
-//         <Route path="/admin" element={<Adminpackage />} />
-//         <Route path="/payment" element={<PaymentPage1 />} />
-//         <Route path="/chat" element={<Chat />} />
-//         <Route path="/test" element={<Test1 />} />
-//  <Route path="/Complaint" element={<AdminComplaintListPage />} />
-{
-  /* <Route path="/Complaint/:id" element={<ComplaintSeeDetailsPage />} /> */
-} //       </Routes>
-//     </>
-//   );
-// }
-// export default App;
+        {/* <Route path="/Complaint/:id" element={<ComplaintSeeDetailsPage />} />  */}
+      </Routes>
+    </>
+  );
+}
+export default App;
 
-// import AuthenticatedApp from "./pages/AuthenticatedApp";
-// import UnauthenticatedApp from "./pages/UnauthenticatedApp";
-// import { useAuth } from "./contexts/authentication";
+//  import AuthenticatedApp from "./pages/AuthenticatedApp";
+//  import UnauthenticatedApp from "./pages/UnauthenticatedApp";
+//  import { useAuth } from "./contexts/authentication";
 
-// function App() {
-//   const auth = useAuth();
-//   return auth.isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />;
-// }
-// export default App;
+//  function App() {
+//    const auth = useAuth();
+//    return auth.isAuthenticated ? <AuthenticatedApp /> : <UnauthenticatedApp />;
+//  }
+//  export default App;
