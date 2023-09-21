48ee7774247f128f9c6da0b70cbd38757decc529
Panpaphop <panpaphop9@gmail.com>
2023-09-21 23:49:10 +0700

    feat: admin can click cancel/resolved button to change complaint status

--- client/src/components/AdminComplaintDetail.jsx
+++ client/src/components/AdminComplaintDetail.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios"; // Import Axios
+import axios from "axios";
+import arrow_back from "../assets/icon/arrow_back_gray_500.svg";
+import { Link } from "react-router-dom";
 
 const AdminComplaintDetail = () => {
   const [complaint, setComplaint] = useState({});
   const { complaintId } = useParams();
+  const [statusUpdated, setStatusUpdated] = useState(false);
 
   useEffect(() => {
-    // Make an HTTP GET request to fetch the complaint details using Axios
     axios
       .get(`http://localhost:4000/complaint/${complaintId}`)
       .then((response) => {
@@ -16,14 +18,50 @@ const AdminComplaintDetail = () => {
       })
       .catch((error) => {
         console.error("Error fetching complaint details:", error);
-        // Handle the error as needed
       });
   }, [complaintId]);
+
+  const markComplaintAsCancel = async (complaintId) => {
+    try {
+      await axios.put(`http://localhost:4000/complaint/${complaintId}/status`, {
+        status: "Cancel",
+      });
+
+      setStatusUpdated(true);
+    } catch (error) {
+      console.error("Error updating complaint status:", error);
+    }
+  };
+
+  const markComplaintAsResolved = async (complaintId) => {
+    try {
+      await axios.put(`http://localhost:4000/complaint/${complaintId}/status`, {
+        status: "Resolved",
+      });
+      setStatusUpdated(true);
+    } catch (error) {
+      console.error("Error updating complaint status:", error);
+    }
+  };
+
+  useEffect(() => {
+    // Check if statusUpdated has changed, and if so, reload the page
+    if (statusUpdated) {
+      window.location.reload();
+    }
+  }, [statusUpdated]);
 
   return (
     <div>
       <section className="w-full flex justify-between h-[10%] px-[60px] py-[16px] ">
         <div className="flex justify-center items-center font-bold text-2xl ">
+          <Link to="/complaint">
+            <img
+              src={arrow_back}
+              alt="arrow_back"
+              className="mr-[16px] w-[24px] h-[24px]"
+            />
+          </Link>
           {complaint.issue}
           <span
             className={` ml-[16px] font-normal ${
@@ -46,12 +84,14 @@ const AdminComplaintDetail = () => {
           <button
             type="button"
             className="flex flex-col justify-center px-[24px] py-[12px] mr-4 rounded-full bg-red-100 text-red-500 drop-shadow-md hover:bg-red-600 hover:text-white"
+            onClick={() => markComplaintAsCancel(complaint.complaint_id)}
           >
             Cancel Complaint
           </button>
           <button
             type="submit"
             className="flex flex-col justify-center px-[24px] py-[12px] rounded-full bg-red-500 text-white drop-shadow-md hover:bg-red-600 hover:text-white"
+            onClick={() => markComplaintAsResolved(complaint.complaint_id)}
           >
             Resolve Complaint
           </button>
@@ -66,7 +106,9 @@ const AdminComplaintDetail = () => {
             </span>
             <hr className="h-px my-8 bg-gray-300 border-0 dark:bg-gray-700 w-[100%]"></hr>
             <span className="mb-[8px]">Issue</span>
-            <div className="text-[16px] text-black mb-[40px]">{complaint.issue}</div>
+            <div className="text-[16px] text-black mb-[40px]">
+              {complaint.issue}
+            </div>
             <span className="mb-[8px]">Description</span>
             <div className="text-[16px] text-black mb-[40px]">
               {complaint.description}
