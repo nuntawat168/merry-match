8ea774e6bacbc7851aa5bd3d366b91b4b9d1028f
Ice Nantawat <nuntawat_168@outlook.com>
2023-09-27 16:29:13 +0700

    feat: add 1-1 chat

--- client/src/components/DiscoverSideBar.jsx
+++ client/src/components/DiscoverSideBar.jsx
@@ -1,150 +1,188 @@
-import React from 'react';
-import discoverIcon from '../assets/icon/discover.svg';
-import axios from 'axios';
-import { useState, useEffect } from 'react';
-import jwtDecode from 'jwt-decode';
+import React from "react";
+import discoverIcon from "../assets/icon/discover.svg";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import jwtDecode from "jwt-decode";
 import merryHeart from "../assets/icon/merry.png";
-import { io } from 'socket.io-client';
-import { Link } from 'react-router-dom';
+import { useSocket } from "../contexts/socketContext";
+import { useNavigate } from "react-router-dom";
+import { useMatch } from "../contexts/matchContext";
 
 const DiscoverSideBar = () => {
-    const [matchList, setMatchList] = useState([]);
-    const [displayedUsers, setDisplayedUsers] = useState([]);
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [conversations, setConversations] = useState([]);
+  const { socket, room, setRoom } = useSocket();
+  const navigate = useNavigate();
+  const { setContentToRender } = useMatch();
 
-    const socket = io("http://localhost:4000");
+  const [matchList, setMatchList] = useState([]);
+  const [displayedUsers, setDisplayedUsers] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [conversations, setConversations] = useState([]);
 
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        const user = jwtDecode(token);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const user = jwtDecode(token);
 
-        axios.get(`http://localhost:4000/user/conversationlist/${user.id}`)
-            .then((response) => {
-                setConversations(response.data.data);
-            })
-            .catch((error) => {
-                console.error('Error fetching data:', error);
-            });
-    }, []);
+    axios
+      .get(`http://localhost:4000/user/conversationlist/${user.id}`)
+      .then((response) => {
+        setConversations(response.data.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-    conversations.forEach(conversation => {
-        const { conversation_id, client1_id, client2_id } = conversation;
-        const roomID = `conversation_${conversation_id}`;
+  useEffect(() => {
+    if (matchList.length > 0) {
+      setDisplayedUsers(matchList.slice(currentIndex, currentIndex + 2));
+    }
+  }, [matchList, currentIndex]);
 
-        socket.emit("joinRoom", { roomID, users: [client1_id, client2_id] });
-    });
+  const handleNextClick = () => {
+    if (currentIndex + 2 < matchList.length) {
+      setCurrentIndex(currentIndex + 2);
+    }
+  };
 
+  const handlePrevClick = () => {
+    if (currentIndex >= 2) {
+      setCurrentIndex(currentIndex - 2);
+    }
+  };
 
-    useEffect(() => {
-        if (matchList.length > 0) {
-            setDisplayedUsers(matchList.slice(currentIndex, currentIndex + 2));
-        }
-    }, [matchList, currentIndex]);
-
-
-    const handleNextClick = () => {
-        if (currentIndex + 2 < matchList.length) {
-            setCurrentIndex(currentIndex + 2);
-        }
+  const handleStartChat = (conversationAndUserInfo) => {
+    const conversation = {
+      client1_id: conversationAndUserInfo.client1_id,
+      client2_id: conversationAndUserInfo.client2_id,
+      conversation_id: conversationAndUserInfo.conversation_id,
     };
+    socket.emit("joinRoom", { conversation });
+    setRoom(conversation);
+    setContentToRender(`Chat-${conversation.conversation_id}`);
+  };
 
-    const handlePrevClick = () => {
-        if (currentIndex >= 2) {
-            setCurrentIndex(currentIndex - 2);
-        }
-    };
+  const fetchMatchList = async (user_id) => {
+    try {
+      const token = localStorage.getItem("token");
+      const user = jwtDecode(token);
+      const response = await axios.get(
+        `http://localhost:4000/user/matchlist/${user_id}`
+      );
+      return response.data.data;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  };
 
-    const handleStartChat = (conversation_id, users) => {
-        const roomID = `conversation_${conversation_id}`;
-        socket.emit("joinRoom", { roomID, users });
+  const fetchData = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const user = jwtDecode(token);
+      const data = await fetchMatchList(user.id);
+      setMatchList(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-        return <Link to={`/chat/${roomID}`}></Link>;
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchMatchList = async (user_id) => {
-        try {
-            const token = localStorage.getItem('token');
-            const user = jwtDecode(token);
-            const response = await axios.get(`http://localhost:4000/user/matchlist/${user_id}`);
-            return response.data.data;
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            throw error;
-        }
-    };
-
-
-
-    const fetchData = async () => {
-        try {
-            const token = localStorage.getItem("token");
-            const user = jwtDecode(token);
-            const data = await fetchMatchList(user.id);
-            setMatchList(data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-
-
-
-    return (
-        <div className='h-[936px] bg-[#ffff] w-[316px] flex flex-col justify-between'>
-            <button className='flex flex-col justify-center items-center w-[275px] p-2 border-[2px] rounded-xl border-purple-500 m-[20px] hover:bg-purple-200'>
-                <img className='w-[66.33px] mt-[30px]' src={discoverIcon} alt="Discover Icon" />
-                <p className='text-[24px] font-bold text-red600'>Discover New Match</p>
-                <p className='text-[14px] mb-[30px] text-gray-600 text-center'>Start find and Merry to get know and connect with new friend!</p>
-            </button>
-            <div>
-                <p className='text-left ml-[20px] font-bold text-[24px]'>
-                    Merry Match!
-                </p>
-                <div className="flex flex-wrap">
-                    {displayedUsers.map((user) => (
-                        <div key={user.id} className='flex'>
-                            <button>
-                                <div className='relative'>
-                                    <div className='flex justify-end relative left-[75px] bottom-[-100px] w-[34px] h-[20px]'>
-                                        <img src={merryHeart} alt="heart" className='z-10 absolute right-[10px] w-[20px] h-[20px]' />
-                                        <img src={merryHeart} alt="heart" className='z-30 w-[20px] h-[20px]' />
-                                    </div>
-                                    <img src={user.image[0].url} alt={user.name} className='w-[100px] h-[100px] rounded-xl ml-[10px]' />
-                                </div>
-                            </button>
-                        </div>
-                    ))}
+  return (
+    <div className="h-[936px] bg-[#ffff] w-[316px] flex flex-col justify-between">
+      <button
+        onClick={() => {
+          setContentToRender("Matching");
+        }}
+        className="flex flex-col justify-center items-center w-[275px] p-2 border-[2px] rounded-xl border-purple-500 m-[20px] hover:bg-purple-200"
+      >
+        <img
+          className="w-[66.33px] mt-[30px]"
+          src={discoverIcon}
+          alt="Discover Icon"
+        />
+        <p className="text-[24px] font-bold text-red600">Discover New Match</p>
+        <p className="text-[14px] mb-[30px] text-gray-600 text-center">
+          Start find and Merry to get know and connect with new friend!
+        </p>
+      </button>
+      <div>
+        <p className="text-left ml-[20px] font-bold text-[24px]">
+          Merry Match!
+        </p>
+        <div className="flex flex-wrap">
+          {displayedUsers.map((user, index) => (
+            <div key={index} className="flex">
+              <button>
+                <div className="relative">
+                  <div className="flex justify-end relative left-[75px] bottom-[-100px] w-[34px] h-[20px]">
+                    <img
+                      src={merryHeart}
+                      alt="heart"
+                      className="z-10 absolute right-[10px] w-[20px] h-[20px]"
+                    />
+                    <img
+                      src={merryHeart}
+                      alt="heart"
+                      className="z-30 w-[20px] h-[20px]"
+                    />
+                  </div>
+                  <img
+                    src={user.image[0].url}
+                    alt={user.name}
+                    className="w-[100px] h-[100px] rounded-xl ml-[10px]"
+                  />
                 </div>
-
-                <div className="flex justify-between items-center p-4">
-                    <button onClick={handlePrevClick} disabled={currentIndex === 0} className="text-xl">Previous</button>
-                    <button onClick={handleNextClick} disabled={currentIndex + 2 >= matchList.length} className="text-xl">Next</button>
-                </div>
+              </button>
             </div>
-            <div>
-                <p className='text-left ml-[20px] font-bold text-[24px]'>
-                    Chat with Merry Match
-                </p>
-                <div>
-                    <ul>
-                        {conversations.map((conversation) => (
-                            <li key={conversation.conversation_id}>
-                                <button type="button" onClick={() => handleStartChat(conversation.conversation_id, [conversation.client1_id, conversation.client2_id])}>
-                                    {conversation.name} - {conversation.about_me}
-                                    <img src={conversation.image[0].url} alt={conversation.name} className='w-[100px] h-[100px] rounded-xl ml-[10px]' />
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
+          ))}
         </div>
-    )
-}
+
+        <div className="flex justify-between items-center p-4">
+          <button
+            onClick={handlePrevClick}
+            disabled={currentIndex === 0}
+            className="text-xl"
+          >
+            Previous
+          </button>
+          <button
+            onClick={handleNextClick}
+            disabled={currentIndex + 2 >= matchList.length}
+            className="text-xl"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+      <div>
+        <p className="text-left ml-[20px] font-bold text-[24px]">
+          Chat with Merry Match
+        </p>
+        <div>
+          <ul>
+            {conversations.map((conversation, index) => (
+              <li key={index}>
+                <button
+                  type="button"
+                  onClick={() => handleStartChat(conversation)}
+                >
+                  {conversation.name} - {conversation.about_me}
+                  <img
+                    src={conversation.image[0].url}
+                    alt={conversation.name}
+                    className="w-[100px] h-[100px] rounded-xl ml-[10px]"
+                  />
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default DiscoverSideBar;
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,91 +1,128 @@
-import DiscoverSideBar from '../components/discoverSideBar';
-import Navbar from '../components/Navbar';
 import sendIcon from "../assets/icon/send button.png";
 import uploadPhotoIcon from "../assets/icon/upload image.png";
 import merryHeart from "../assets/icon/merry.png";
-import { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useSocket } from "../contexts/socketContext";
+import jwtDecode from "jwt-decode";
 
-function Chat() {
-    const mockData = { username: "Daeny" }
-    const socket = io('http://localhost:4000');
+function Chat(props) {
+  const token = localStorage.getItem("token");
+  const user = jwtDecode(token);
+  const { socket, messages, setMessages, room } = useSocket();
 
-    const [messages, setMessages] = useState([]);
-    const [inputMessage, setInputMessage] = useState('');
+  const [inputMessage, setInputMessage] = useState("");
+  const [senderId, setSenderId] = useState();
+  const [receiverId, setReceiverId] = useState();
 
+  function sendMessage() {
+    const token = localStorage.getItem("token");
+    const user = jwtDecode(token);
 
+    const newMessage = {
+      sender_id: senderId,
+      receiver_id: receiverId,
+      text: inputMessage,
+      conversation_id: room.conversation_id,
+    };
+    socket.emit("send-message", newMessage);
 
+    setMessages((message) => [...message, newMessage]);
+    setInputMessage("");
 
+    return true;
+  }
+
+  useEffect(() => {
+    setSenderId(user.id);
+    if (room.client1_id !== user.id) {
+      setReceiverId(room.client1_id);
+    } else if (room.client2_id !== user.id) {
+      setReceiverId(room.client2_id);
+    }
+  }, [props.room]);
+
+  function StartMatchingMsg() {
+    {
+      /* show matching matching msg */
+    }
     return (
-        <main className='bg font-nunito w-full'>
-            <Navbar />
-            <div className='flex'>
-                <DiscoverSideBar />
-                <div className='text-white w-full relative'>
-                    <section className='flex flex-col px-[60px]'>
-                        {/* show matching matching msg */}
-                        <div className='bg-purple-100 w-[749px] h-[90px] p-[24px] rounded-2xl flex justify-center text-red-700 text-[14px] font-semibold mt-[48px] absolute top-[50px] left-1/2 transform -translate-x-1/2'>
-                            <div className='flex justify-end relative'>
-                                <img src={merryHeart} alt="heart" className='z-10 absolute right-[20px] w-[35px] h-[35px]' />
-                                <img src={merryHeart} alt="heart" className='z-30 w-[35px] h-[35px]' />
-                            </div>
-                            <div className='pl-[24px]'>
-                                <p>Now you and {mockData.username} are Merry Match!</p>
-                                <p>You can message something nice and make a good conversation. Happy Merry!</p>
-                            </div>
-                        </div>
+      <div className="bg-purple-100 w-[749px] h-[90px] p-[24px] rounded-2xl flex justify-center text-red-700 text-[14px] font-semibold mt-[48px] absolute top-[50px] left-1/2 transform -translate-x-1/2">
+        <div className="flex justify-end relative">
+          <img
+            src={merryHeart}
+            alt="heart"
+            className="z-10 absolute right-[20px] w-[35px] h-[35px]"
+          />
+          <img
+            src={merryHeart}
+            alt="heart"
+            className="z-30 w-[35px] h-[35px]"
+          />
+        </div>
+        <div className="pl-[24px]">
+          <p>Now you and {mockData.username} are Merry Match!</p>
+          <p>
+            You can message something nice and make a good conversation. Happy
+            Merry!
+          </p>
+        </div>
+      </div>
+    );
+  }
 
-                        {/* left side chat */}
+  function renderMsg(conversation, index) {
+    if (conversation.sender_id !== user.id) {
+      return (
+        <div className="text-[16px] text-black flex items-center" key={index}>
+          <div className="w-[40px] h-[40px] rounded-full bg-red-500 mr-[12px]"></div>
+          <p className="py-[16px] px-[24px] bg-purple-200 rounded-tl-[24px] rounded-tr-[24px] rounded-br-[24px] rounded-bl-[0px] border-[1px] border-gray-300 ">
+            {conversation.text}
+          </p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex flex-col items-end" key={index}>
+          <div className="w-auto py-[16px] px-[24px] bg-purple-600 rounded-tl-[24px] rounded-tr-[24px] rounded-br-[0px] rounded-bl-[24px]">
+            {conversation.text}
+          </div>
+        </div>
+      );
+    }
+  }
 
-                        <div className='mb-[48px] mt-[236px]'>
-                            <div className='text-[16px] text-black flex items-center'>
-                                <div className='w-[40px] h-[40px] rounded-full bg-red-500 mr-[12px]'></div>
-                                <p className='py-[16px] px-[24px] bg-purple-200 rounded-tl-[24px] rounded-tr-[24px] rounded-br-[24px] rounded-bl-[0px] border-[1px] border-gray-300 mb-[16px]'>Hi</p>
-                            </div>
-                            <div className='text-[16px] text-black flex items-center'>
-                                <div className='w-[40px] h-[40px] rounded-full bg-red-500 mr-[12px]'></div>
-                                <p className='py-[16px] px-[24px] bg-purple-200 rounded-tl-[24px] rounded-tr-[24px] rounded-br-[24px] rounded-bl-[0px] border-[1px] border-gray-300 mb-[16px]'>Do you like ma dragons?</p>
-                            </div>
-                            <div className='text-[16px] text-black flex items-end'>
-                                <div className='w-[40px] h-[40px] rounded-full bg-red-500 mr-[12px]'></div>
-                                <div className='w-[240px] h-[240px] bg-red-200 rounded-2xl'></div>
-                            </div>
+  return (
+    <div className="text-white w-full border border-red flex flex-col justify-end ">
+      <section className="h-full flex flex-col justify-end px-[60px] space-y-4 pt-[98px] pb-10">
+        {messages.map((conversation, index) => {
+          return renderMsg(conversation, index);
+        })}
+      </section>
 
-                        </div>
-                        {/* right side chat */}
-                        <div className='text-[16px] text-white'>
-                            <div className='flex flex-col items-end'>
-
-                                {messages.map((message, index) => (
-                                    <div className='w-auto py-[16px] px-[24px] bg-purple-600 rounded-tl-[24px] rounded-tr-[24px] 
-                                rounded-br-[0px] rounded-bl-[24px] mb-[16px]' key={index}>{message}</div>))}
-                            </div>
-                        </div>
-                    </section>
-
-                    {/* text input field */}
-                    <section className='bg border-t-[1px] border-gray-800 h-[100px] px-[60px] w-full flex justify-between items-center absolute bottom-0'>
-                        <div className='flex items-center'>
-                            <img src={uploadPhotoIcon} alt="upload photo icon" className='w-[48px] h-[48px]' />
-                            <input
-                                className='text-gray-500 text-[16px] pl-[24px]'
-                                type='text'
-                                value={inputMessage}
-                                onChange={(e) => setInputMessage(e.target.value)}
-                                placeholder='Message here...' />
-                        </div>
-                        <div>
-                            <button onClick={sendMessage}>
-                                <img src={sendIcon} alt="send icon" className='w-[48px] h-[48px]' />
-                            </button>
-                        </div>
-                    </section>
-                </div>
-            </div>
-        </main>
-    )
+      {/* text input field */}
+      <section className="bg border-t-[1px] border-gray-800 h-[100px] px-[60px] w-full flex justify-between items-center">
+        <div className="flex items-center">
+          <img
+            src={uploadPhotoIcon}
+            alt="upload photo icon"
+            className="w-[48px] h-[48px]"
+          />
+          <input
+            className="text-gray-500 text-[16px] pl-[24px]"
+            type="text"
+            value={inputMessage}
+            onChange={(e) => setInputMessage(e.target.value)}
+            placeholder="Message here..."
+          />
+        </div>
+        <div>
+          <button onClick={sendMessage}>
+            <img src={sendIcon} alt="send icon" className="w-[48px] h-[48px]" />
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Chat;
