41720af44eb5b222210e2315d100f4d4c685b12b
Panpaphop <panpaphop9@gmail.com>
2023-09-28 01:57:36 +0700

    feat: show modal and redirect user to match page when user make a complaint

--- client/src/pages/UserComplaint.jsx
+++ client/src/pages/UserComplaint.jsx
@@ -1,16 +1,31 @@
-import React, { useContext } from "react";
-import { Formik, Field, Form, ErrorMessage } from "formik";
+import React, { useContext, useRef, useState } from "react";
+import { Formik, Field, Form, ErrorMessage, useFormik } from "formik";
 import * as Yup from "yup";
 import manImage from "../../src/assets/image/manOnLoginPage.svg";
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import {
+  useDisclosure,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  AlertDialogCloseButton,
+  Divider,
+} from "@chakra-ui/react";
 
 function UserComplaintPage() {
   const token = localStorage.getItem("token");
   const user = jwtDecode(token);
   const user_id = user.id;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
+  const navigate = useNavigate();
   const initialValues = {
     issue: "",
     description: "",
@@ -24,6 +39,7 @@ function UserComplaintPage() {
   });
 
   const onSubmit = async (values) => {
+    console.log(values);
     values.user_id = user_id;
 
     try {
@@ -31,7 +47,7 @@ function UserComplaintPage() {
         "http://localhost:4000/complaint",
         values
       );
-      window.alert(`${response.data.message}`);
+      // window.alert(`${response.data.message}`);
       console.log(
         "Data posted to http://localhost:4000/complaint:",
         response.data
@@ -41,8 +57,9 @@ function UserComplaintPage() {
         "Error posting data to http://localhost:4000/complaint:",
         error
       );
-      window.alert(`${response.data.error}`);
+      window.alert(`${error.response.data.error}`);
     }
+    onOpen();
   };
 
   return (
@@ -134,9 +151,58 @@ function UserComplaintPage() {
               <button
                 type="submit"
                 className="w-[102px] h-[48px] rounded-full bg-red-500 hover:bg-red-600 focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 shadow-md text-white mt-[58px]"
+                // onClick={onOpen}
               >
                 Submit
               </button>
+
+              <AlertDialog
+                motionPreset="slideInBottom"
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+                isOpen={isOpen}
+                isCentered
+              >
+                <AlertDialogOverlay />
+                <AlertDialogContent borderRadius="3xl" maxW="528px">
+                  <AlertDialogHeader fontSize={20}>
+                    Complaint created successfully!
+                  </AlertDialogHeader>
+                  <AlertDialogCloseButton
+                    color="gray.500"
+                    marginX={2}
+                    marginY={2}
+                  />
+                  <Divider marginBottom={5} borderColor="gray.300"></Divider>
+                  <AlertDialogBody
+                    fontSize={16}
+                    color="gray.600"
+                    marginBottom="10px"
+                  >
+                    Do you want to go back to matching page?
+                  </AlertDialogBody>
+                  <AlertDialogFooter justifyContent="flex-start">
+                    <button
+                      type="submit"
+                      className="py-[12px] mb-[15px] px-[24px] text-[16px] font-semibold rounded-[99px] bg-red-100 text-red-600 shadow-btn"
+                      ref={cancelRef}
+                      onClick={() => navigate("/match")}
+                    >
+                      Yes, I do.
+                    </button>
+                    <button
+                      className="ml-4 mb-[15px] py-[12px] px-[24px] text-[16px] font-semibold rounded-[99px] bg-red-500 text-white shadow-login"
+                      ref={cancelRef}
+                      onClick={() => {
+                        onClose();
+                        window.location.reload();
+                      }}
+                    >
+                      No, I want to make more complaint.
+                    </button>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
             </Form>
           </Formik>
         </div>
