3585093b3b144a12b51c902313d75d8926d6994b
CodePawsDev <isripen1991@outlook.com>
2023-09-29 17:03:13 +0700

    feat: add more reliable payment

--- client/src/components/StripePayment.jsx
+++ client/src/components/StripePayment.jsx
@@ -17,7 +17,7 @@ export default function StripePayment() {
 
   const createPaymentIntent = async () => {
     const data = await axios.post(
-      'http://localhost:4000/payment/create-payment-intent', {price: dataPackage[savePackage].package_price});
+      'http://localhost:4000/payment/create-payment-intent', { price: dataPackage[savePackage].package_price} );
     console.log(data.data);
     setClientSecret(data.data.clientSecret);
   };
